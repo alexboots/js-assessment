7e3b5295bf67c7265f1b248f7d05f2e6321e8d14
Alex Hays <alexhays@gmail.com>
2018-01-24 12:14:57 -0500

    do timeout one

--- app/count.js
+++ app/count.js
@@ -2,6 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let timeout
+    let count = start
+    
+    function tick () {
+      if (count <= end) {
+        console.log(count)
+        count += 1
+        timeout = setTimeout(tick, 100)
+      }
+    }
 
+    tick()
+
+    function cancel() {
+      clearTimeout(timeout)
+    }
+
+    return { cancel }
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    console.log('str', str);
+    console.log('amount', amount);
   },
 
   wordWrap: function(str, cols) {
-
+    console.log('str', str);
+    console.log('cols', cols);
   },
 
   reverseString: function(str) {
-
+    return str.split('').reverse().join('')
   }
 };
